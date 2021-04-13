c2e350c7da436e622095ac7c45405913cfabf48f
Jan Amann <jan@amann.me>
2021-04-13 16:10:20 +0200

    feat: use caritas frontend lib for basic setup

---
+++ src/components/app/initApp.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => React.createElement('p', null, 'App');
+
+ReactDOM.render(React.createElement(App), document.getElementById('appRoot'));
---
+++ src/components/error/initError.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const Login = () => React.createElement('p', null, 'Login');
+
+ReactDOM.render(
+	React.createElement(Login),
+	document.getElementById('errorRoot')
+);
